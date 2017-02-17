2c18cf33d89452013b683777a44484085cdd5dd1
luandoan <luandoan263@gmail.com>
2017-02-17 17:25:16 -0500

    detect corrected, depth still in progress

--- src/depthsense_cam/script/circle_ds.py
+++ src/depthsense_cam/script/circle_ds.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 
-""" circle_detect using Depth Sense camera
-    based on HoughCircles python ROS
+"""	circle_detect using Depth Sense camera
+	based on HoughCircles python ROS
+	Detect all possible circles and point out the interested one, which closest
+	to center of image frame or any other interest points.
+	Special applied to CCAM project and End-Effector design
 
-    Detect all possible circles and point out the interested one, which closest
-    to center of image frame or any other interest points.
-    Special applied to CCAM project and End-Effector design
-
-    Created by Luan Doan
-    CMS Lab, VTech June 2016
-
+	Created by Luan Doan
+	CMS Lab, VTech June 2016
 """
 
 import rospy
@@ -21,88 +19,122 @@ import time
 import numpy as np
 
 from sensor_msgs.msg import Image
-from std_msgs.msg import String
+from std_msgs.msg import String, MultiArrayLayout, MultiArrayDimension
+from rospy_tutorials.msg import Floats
 from cv_bridge import CvBridge, CvBridgeError
-
-# function calculate distance from 2 points in xOy frame
-def distance(x1,y1,x2,y2):
-    xd = x1 - x2
-    yd = y1 - y2
-    return math.sqrt(xd*xd + yd*yd)
 
 
 class circle_detect:
+	def __init__(self):
+		self.image_pub = rospy.Publisher("circle_detection", Image, queue_size = 10)
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("/rgb_image", Image, self.callback)
+		self.depth_sub = rospy.Subscriber("/depth_image", Image, self.depth_img)
+		self.point_pub = rospy.Publisher("point_support", Floats, queue_size = 5)
 
-    def __init__(self):
-        self.image_pub = rospy.Publisher("circle_detection", Image, queue_size = 10)
+	def callback(self, data):
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+		sz = np.shape(cv_image)
 
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/rgb_image", Image, self.callback)
+		# Actual content code
+		#  code fiding circles and publish the closest circle
+		x0 = 320
+		y0 = 240
+		idx = None
+		minC = 800
+		numC = None
+		
+		img = cv2.medianBlur(cv_image,5)
+		imgg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+		
+		cimg = cv2.cvtColor(imgg, cv2.COLOR_GRAY2BGR)
+		circles = cv2.HoughCircles(imgg,cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100,  param1 = 200, param2 = 30, minRadius = 5, maxRadius = 50)
 
-    def callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
+		if circles is  not None:
+			# get the number of circles detected
+			sp = np.shape(circles)
+			nc = sp[1]
+			rospy.loginfo("Circle found: %d", nc)
+			#print nc
+		
+			#find the closest circle regarding center point
+			data = np.uint16(np.around(circles)).reshape(-1,3)
+			dists = np.zeros(nc)
+			mindist = 2000
+			idx = 0
+			for i in range(nc):
+				dists[i] = np.sqrt((data[i,0]-sz[1]/2)**2 + (data[i,1]-sz[0]/2)**2)
+				if (dists[i] < mindist):
+					mindist = dists[i]
+					idx = i
+			best = data[idx]
+			print "The closest circle: ", np.round(mindist), idx, best 
+				
+			# Draw selected circle and point out some information
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			cv2.putText(cv_image, 'Image size is: ' + str(sz[0]) + "x" + str(sz[1]),  (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0)) 
+			cv2.putText(cv_image, 'Number of detected circles: ' + str(nc),  (10,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0)) 
+			cv2.putText(cv_image, 'The selected wrench position is: ' + str(best[0]) + "x" + str(best[1]),  (10,90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0)) 
+			
+			# draw the center and outer circle
+			cv2.circle(cv_image, (best[0],best[1]), 2, (0,0,255), 3)
+			cv2.circle(cv_image, (best[0],best[1]), best[2], (0,255,0), 2)
 
-        # Actual content code
-        #  code fiding circles and publish the closest circle
-        x0 = 320
-        y0 = 240
-        idx = None
-        minC = 800
-        numC = None
+			# pick 3 random points on detected circle
+			cv2.circle(cv_image, (best[0],best[1]-best[2]), 2, (0,0,255), 3)
+			cv2.circle(cv_image, (best[0]-best[2],best[1]), 2, (0,0,255), 3)
+			cv2.circle(cv_image, (best[0]+best[2],best[1]), 2, (0,0,255), 3)
 
-        img = cv2.medianBlur(cv_image,5)
-        imgg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+			# pick 3 random points closed to circle
+			cv2.circle(cv_image, (best[0],best[1]-best[2]-10), 2, (255,0,0), 3)
+			cv2.circle(cv_image, (best[0]-best[2]-10,best[1]), 2, (255,0,0), 3)
+			cv2.circle(cv_image, (best[0]+best[2]+10,best[1]), 2, (255,0,0), 3)
 
-        cimg = cv2.cvtColor(imgg, cv2.COLOR_GRAY2BGR)
-        circles = cv2.HoughCircles(imgg,cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100,  param1 = 200, param2 = 30, minRadius = 5, maxRadius = 50)
+			# pick 3 random points closed to circle
+			cv2.circle(cv_image, (best[0],best[1]+best[2]+20), 2, (255,255,0), 3)
+			cv2.circle(cv_image, (best[0]-best[2]-20,best[1]), 2, (255,255,0), 3)
+			cv2.circle(cv_image, (best[0]+best[2]+20,best[1]), 2, (255,255,0), 3)
+
+			# publish interested points to array
+			point = np.array([ best[0],best[1]-best[2], best[0]-best[2],best[1], best[0]+best[2],best[1], best[0],best[1]-best[2]-10, best[0],best[1]+best[2]+10, best[0],best[1]+best[2]+20, best[0]-best[2]-20,best[1], best[0]+best[2]+20,best[1]])
+			self.point_pub.publish(point)
+				
+
+		else:
+			cv2.imshow("Image Windows", cv_image)
+			rospy.loginfo('No circles detected')
+			
+		
+		cv2.imshow("Image Windows", cv_image)
+		cv2.waitKey(3)
+
+		# process depth image
+		#depth_data = self.depth_img(data)
+		#cv_depth = self.bridge.imgmsg_to_cv2(depth_data, "32FC1")
+		#cv2.imshow("Depth Image", cv_depth)		
 
 
-        if circles is None:
-           cv2.imshow("Image Windows", cv_image)
-           rospy.loginfo('No circles detected')
-        #circles = np.uint16(np.around(circles))  # .astype("int")     # convert the (x,y) coordinates and radius of the circles to integers
-        #numC = len(circles[0,:])
-        else:
-            print circles.shape
+		try:
+			self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+			self.point_pub.publish(point)
+		except CvBridgeError as e:
+			print(e)
 
-
-        # rospy.loginfo('circles found, number of circles {}'.format(numC))
-
-        # for i in circles:
-        #     dist[i] = distance(x0,y0, i[0],i[1])
-        #     if dist[i] < minC:
-        #         minC = dist[i]
-        #         idx = i
-
-        #for i in circles[0,:]:
-        # print idx
-        # cv2.circle(cv_image, (idx[0],idx[1]),idx[2], (0,255,0),1)     # draw the outer cirlce
-        # cv2.circle(cv_image, (idx[0],idx[1]), 2, (0,0,255), 3)      # draw the center of the circle
-
-            for i in circles[0,:]:
-                cv2.circle(cv_image, (i[0],i[1]),i[2], (0,255,0),1)     # draw the outer cirlce
-                cv2.circle(cv_image, (i[0],i[1]), 2, (0,0,255), 3)      # draw the center of the circle
-
-        cv2.imshow("Image Windows", cv_image)
-        cv2.waitKey(3)
-
-        try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-        except CvBridgeError as e:
-            print(e)
-
+	def depth_img(self,data):
+		self.data = data
+		return data
 
 def main(args):
-    ct = circle_detect()
-    rospy.init_node('circle_detection', anonymous=True)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
-    cv2.destroyAllWindows()
+	ct = circle_detect()
+	rospy.init_node('circle_detection', anonymous=True)
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
+	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main(sys.argv)
+	main(sys.argv)
--- src/depthsense_cam/script/depth_info.py
+++ src/depthsense_cam/script/depth_info.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 
-""" circle_detect using Depth Sense camera
-    based on HoughCircles python ROS
-
-    Detect all possible circles and point out the interested one, which closest
-    to center of image frame or any other interest points.
-    Special applied to CCAM project and End-Effector design
-
-    Created by Luan Doan
-    CMS Lab, VTech June 2016
+""" 	distance definition
+	get distance from some interest points for getting orientation
+	and adjusting values 
+	Created by Luan Doan
+	CMS Lab, VTech June 2016
 """
 
 import rospy
@@ -22,77 +18,25 @@ import numpy as np
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
+from rospy_tutorials.msg import Floats
+class depth_distance:
+	def __init__(self):
+		self.distance_pub = rospy.Publisher("/point_distance", Floats, queue_size = 10)
+		self.bridge = CvBridge()
+		self.depth_sub = rospy.Subscriber("/depth_image", Image, self.callback)
+		self.point_sub = rospy.Subscriber("/point_support", Float, self.update_pos)
 
-# function calculate distance from 2 points in xOy frame
-def distance(x1,y1,x2,y2):
-    xd = x1 - x2
-    yd = y1 - y2
-    return math.sqrt(xd*xd + yd*yd)
+	def callback(self, data):
+		try:
+			cv_depth = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+		if cv_depth is not None:
+			cv2.imshow("Depth Image", cv_depth)
+			cv2.waitKey(3)
 
-
-class circle_detect:
-
-    def __init__(self):
-        self.image_pub = rospy.Publisher("circle_detection", Image, queue_size = 10)
-
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/rgb_image", Image, self.callback)
-
-    def callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
-
-        # Actual content code
-        #  code fiding circles and publish the closest circle
-        x0 = 320
-        y0 = 240
-        idx = None
-        minC = 800
-        numC = None
-
-        img = cv2.medianBlur(cv_image,5)
-        imgg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-        cimg = cv2.cvtColor(imgg, cv2.COLOR_GRAY2BGR)
-        circles = cv2.HoughCircles(imgg,cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100,  param1 = 200, param2 = 30, minRadius = 5, maxRadius = 20)
-
-
-        if circles is None:
-           cv2.imshow("Image Windows", cv_image)
-           rospy.loginfo('No circles detected')
-        #circles = np.uint16(np.around(circles))  # .astype("int")     # convert the (x,y) coordinates and radius of the circles to integers
-        #numC = len(circles[0,:])
-        else:
-            print circles.shape
-
-
-        # rospy.loginfo('circles found, number of circles {}'.format(numC))
-
-        # for i in circles:
-        #     dist[i] = distance(x0,y0, i[0],i[1])
-        #     if dist[i] < minC:
-        #         minC = dist[i]
-        #         idx = i
-
-        #for i in circles[0,:]:
-        # print idx
-        # cv2.circle(cv_image, (idx[0],idx[1]),idx[2], (0,255,0),1)     # draw the outer cirlce
-        # cv2.circle(cv_image, (idx[0],idx[1]), 2, (0,0,255), 3)      # draw the center of the circle
-
-            for i in circles[0,:]:
-                cv2.circle(cv_image, (i[0],i[1]),i[2], (0,255,0),1)     # draw the outer cirlce
-                cv2.circle(cv_image, (i[0],i[1]), 2, (0,0,255), 3)      # draw the center of the circle
-
-        cv2.imshow("Image Windows", cv_image)
-        cv2.waitKey(3)
-
-        try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-        except CvBridgeError as e:
-            print(e)
-
+	def update_pos(self, pos):
+		self.pos = pos
 
 def main(args):
     ct = circle_detect()
